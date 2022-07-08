a2272fb84d888c60a1844253ba1174b477a932ee
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2022-07-08 23:02:12 +0530

    fix: sign up and login frontend

--- cmd/gnote/views/static/js/app.js
+++ cmd/gnote/views/static/js/app.js
@@ -1,9 +1,13 @@
 // variables
-let username = "mrinjamul";
+let username = "";
 let notes = [];
 
 noteDocument = document.getElementById("notes");
 userDocument = document.getElementById("userinfo");
+
+getData("/user/me").then((data) => {
+  username = data.user.user_name;
+});
 
 const nonotes = `
       <h1 id="nonewnotes">
@@ -106,4 +110,22 @@ function logout() {
   postData("/auth/logout", {}).then((data) => {
     window.location.href = "/";
   });
+}
+
+async function getData(url = "") {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
 }
